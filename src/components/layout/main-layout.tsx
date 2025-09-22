7f71769d3471e9dc60bc8879223a85178fdd5776
Amaan Ahmad <asharamaan234@gmail.com>
2025-09-22 07:51:32 +0000

    Now content are showing but there is sidebar menus and logo are showing

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -1,17 +1,19 @@
 'use client';
 
-import { SidebarProvider } from '@/components/ui/sidebar';
+import { SidebarProvider, Sidebar } from '@/components/ui/sidebar';
 import { AppHeader } from './header';
 import { AppSidebar } from './sidebar';
 
 export default function MainLayout({children}: {children: React.ReactNode}) {
   return (
     <SidebarProvider>
-      <AppSidebar />
       <div className="flex h-screen flex-col">
         <AppHeader />
         <main className="flex-1 overflow-y-auto">{children}</main>
       </div>
+      <Sidebar>
+        <AppSidebar />
+      </Sidebar>
     </SidebarProvider>
   );
 }
